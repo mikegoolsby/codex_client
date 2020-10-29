4da31b7ab156ebd8cb13b595eaeede290541e651
thebialy <67696052+thebialy@users.noreply.github.com>
2020-10-28 19:26:47 -0500

    working on gear card

--- src/Gear.js
+++ src/Gear.js
@@ -1,36 +1,42 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+
 
 const Gear = (props) => {
-
+  
   const {gear} = props
 
+  const classes = useStyles();
+  
   const loaded = () => (
     <div>
       {gear.map((gear) => (
-        <div className="content">
-          <img src={gear.img}/>
-          <h1>{gear.name}</h1>
-          <h3>Price: ›{gear.price}</h3>
-          <p>{gear.description}</p>
-          <h3>Review</h3><p>{gear.review}</p> 
-          <h3><a href= {gear.url}>Buy</a></h3> 
-          <button
-            onClick={() => {
-              props.selectGear(gear);
-              props.history.push("/edit");
-            }}
-          >Edit</button>
-          <button
-            onClick={() => {
-              props.deleteGear(gear);
-            }}
-          >
-            Delete
-          </button>
-        </div>
+        <div class="infocardContainer">
+          <div id="main">
+            <img src={gear.img}></img>
+          </div>
+          <div id="textbois">
+            <h2>{gear.name}</h2>
+            <h4>Price: {gear.price}</h4>
+            <p>{gear.description}</p>
+            <h3>Review</h3><p>{gear.review}</p>
+            
+          </div>
+        </div>  
       ))}
     </div>
   ) 
+  console.log(gear)
   const loading = <h1>Loading...</h1>
   return gear.length > 0 ? loaded() : loading
 };
